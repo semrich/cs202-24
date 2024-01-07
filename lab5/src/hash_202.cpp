3711440667d860ad36bf778dfde89632955d5a08
Scott Joseph Emrich <semrich@helix.eecs.utk.edu>
2024-01-07 11:05:40 -0500

    Adding legacy files for Lab 5 and 6

---
+++ lab5/src/hash_202.cpp
@@ -0,0 +1,32 @@
+#include "hash_202.hpp"
+using namespace std;
+
+string Hash_202::Set_Up(size_t table_size, const std::string &fxn, const std::string &collision)
+{
+  (void) table_size;
+  (void) fxn;
+  (void) collision;
+  return "";
+}
+
+string Hash_202::Add(const string &key, const string &val)
+{
+  (void) key;
+  (void) val;
+  return "";
+}
+
+string Hash_202::Find(const string &key)
+{
+  (void) key;
+  return "";
+}
+
+void Hash_202::Print() const
+{
+}
+
+size_t Hash_202::Total_Probes()
+{
+  return 0;
+}
---
+++ lab6/primes2.cpp
@@ -0,0 +1,8 @@
+// Lab 6: Is it prime?
+
+int main(int argc, char *argv[]) {
+
+  return 0;
+
+}
+
